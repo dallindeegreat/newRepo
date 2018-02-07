e7db3762f4552c52788532f55d191d812314deea
Dallin Deirdre Plaza <dallindeirdre.plaza@yahoo.com>
2018-02-07 08:16:43 +0800

    added

---
+++ addNewBook.php
@@ -0,0 +1,78 @@
+<?php
+require 'include/dbconnection.php'
+    
+?>
+<html>
+
+<head>
+    
+	<title>Book Information</title>
+    <link href="assets/css/reg.css" rel="stylesheet">
+
+</head>
+
+<body>
+	<form action="db.php" method="post">
+	<h1>Library Database</h1>
+        
+	<fieldset>
+		<legend>Book Information</legend>
+		<label>Title:</label> <input type="text" name="title" required/><br />
+		<label>Pages:</label> <input type="number" min=1 name="pages" required/><br />
+		<label>Author:</label> <input type="text" name="author" required/><br />
+		<label>Published Year:</label> <input type="text" name="year" required/>
+        <div><br/></div>
+    <input style="float:right" type="submit" value="Add Book" onClick="return submit_form();" name="submit"/>
+    </fieldset>
+        
+    <h3>List of Stored Books</h3>
+    <table border="2" align="center" cellpadding=5>
+            <thead>
+                <tr><th>Title</th>
+                    <th>Pages</th>
+                    <th>Author</th>
+                    <th>Published Year</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+               <?php
+                              $table  = mysqli_query($dbconn , "SELECT * FROM db");
+                              while($row  = mysqli_fetch_array($table)){ ?>
+                                  <tr id="<?php echo $row['id']; ?>">
+                                    <td data-target="title"><?php echo $row['title']; ?></td>
+                                    <td data-target="pages"><?php echo $row['pages']; ?></td>
+                                    <td data-target="author"><?php echo $row['author']; ?></td>
+                                      <td data-target="year"><?php echo $row['year']; ?></td>
+                                    <td>
+                
+                                 
+                                        
+                                     
+                                      </td>
+                                  </tr>
+                             <?php }
+                                
+                                if (isset($_GET['id1'])) {
+                                    $id1 = $_GET['id1'];
+                                     $result = mysqli_query($dbconn, "DELETE FROM user WHERE id=$id1");
+                                    echo '<script type="text/javascript">
+                                        window.location = "http://localhost/vnc/adminpanel/users.php"
+                                    </script>';
+                                    }
+
+                           ?>
+            </tbody>
+        </table>
+	</form>
+    <script type="text/javascript" src="assets/js/jquery-1.10.2.js"></script>
+	<script type="text/javascript">
+		function submit_form(){
+			alert("New book added");
+		}
+	</script>
+<?php 
+    $dbconn->close();
+?>
+</body>
+</html>
